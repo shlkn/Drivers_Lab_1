177429ae78160169f1cab3f395163e908c790a2e
iliyash <Pro100iliyash@yandex.ru>
2021-10-23 08:47:41 -0700

    made codestyle

--- lab1_2.c
+++ lab1_2.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0
+
 #include <linux/init.h>
 #include <linux/module.h>
 
@@ -14,12 +16,10 @@ static int howmany = 1;
 module_param(howmany, int, 0644);
 MODULE_PARM_DESC(howmany, "Number of greetings");
 
-
 static int __init hello_init(void)
 {
-	int i;
-	for (i = 0; i < howmany; i++)
-	pr_alert("(%d) Hello, %s\n", i, whom);
+	for (int i = 0; i < howmany; i++)
+		pr_alert("(%d) Hello, %s\n", i, whom);
 	return 0;
 }
 
